8ae0d06f6c68c19c705d7c61c00594cad65332ad
Bilal Quraishi <bilal.u.quraishi@gmail.com>
2020-06-13 15:26:10 -0500

    starter project

--- main.c
+++ main.c
@@ -1,11 +1,36 @@
 #include "config.h"
 #include "io.h"
 
-int main(void){
-	//TODO: initialization here
-	while(1){
-		//TODO: main loop here
+bool allGood(void);
+
+int main(void)
+{
+	//initialization
+	currentInit();
+	tempInit();
+	voltInit();
+	contactorInit();
+
+	//initial check before activating
+	if (allGood()) setContactor(true);
+	else return -1;
+	while(1)
+	{
+		//monitor constantly
+		if (!allGood()) 
+		{
+			setContactor(false);
+			return -1;
+		}
 	}
 	return 0;
+}
+
+bool allGood(void)
+{
+	if (currentRead() > MAX_CURRENT) return false;
+	else if (tempRead() > MAX_TEMP) return false;
+	else if (voltRead() < MIN_VOLTAGE || voltRead() > MAX_VOLTAGE) return false;
+	else return true;
 }
 
